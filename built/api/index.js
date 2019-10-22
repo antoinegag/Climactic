2512397830edb1db21727dd035629002eb0cc2b5
Antoine Gagnon <antoine.gagnon760@gmail.com>
2019-10-22 19:16:40 -0400

    Remove ts build directory

--- built/api/index.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-router.get("/", function (req, res) {
-    return res.json({ success: true });
-});
-var stationsRouter = require("./stations");
-router.use("/stations", stationsRouter);
-module.exports = router;
